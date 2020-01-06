dab6b1cf9288b66f649b4fb45f6af77ab7010f83
Einar <einareriksen85@gmail.com>
2020-01-06 14:53:10 +0100

    La til en haug queries

--- SQLQuery9.sql
+++ SQLQuery9.sql
@@ -1,15 +1,43 @@
 /* Alle verdier */
 select * from dbo.lonn2019dummy
 
-/* Lønn for de ulike sektorene */
-select "Lønn" from dbo.lonn2019dummy where Sektor LIKE 'Privat'
-select "Lønn" from dbo.lonn2019dummy where Sektor LIKE 'Stat'
-select "Lønn" from dbo.lonn2019dummy where Sektor LIKE 'Kommune'
-select "Lønn" from dbo.lonn2019dummy
+/* TRENGS IKKE: Lønn for de ulike sektorene */
+select Lønn 
+from dbo.lonn2019dummy 
+where Sektor LIKE 'Privat'
+
+select Lønn 
+from dbo.lonn2019dummy 
+where Sektor LIKE 'Stat'
+
+select Lønn 
+from dbo.lonn2019dummy 
+where Sektor LIKE 'Kommune'
+
+select Lønn 
+from dbo.lonn2019dummy
 
 /* Gjennomsnittslønn gruppert etter sektor */
-SELECT department, AVG(quantity) AS "Average Quantity"
-FROM products
-GROUP BY department;
+SELECT Sektor, AVG(Lønn) AS "Gjennomsnittslønn"
+FROM dbo.lonn2019dummy
+GROUP BY Sektor
+UNION ALL
+SELECT 'Alle' AS Sektor, AVG(Lønn) AS "Gjennomsnittslønn"
+FROM dbo.lonn2019dummy
 
-/* Gjennomsnittslønn gruppert etter sektor */
+/* TRENGS IKKE: Gjennomsnittslønn alle */
+SELECT AVG(Lønn) AS "Gjennomsnittslønn alle"
+FROM dbo.lonn2019dummy
+
+/* Tariffområder */
+SELECT Tariffavtale, AVG(Lønn) AS "Gjennomsnittslønn"
+FROM dbo.lonn2019dummy
+GROUP BY Tariffavtale
+
+/* Privat uten tariffområde */
+SELECT Sektor, AVG(Lønn) AS "Gjennomsnittslønn"
+FROM dbo.lonn2019dummy
+WHERE Tariffavtale IS NULL
+GROUP BY Sektor;
+/* Tror den funker, men har ikke laget noen med sektor privat uten tariffavtale */
+
