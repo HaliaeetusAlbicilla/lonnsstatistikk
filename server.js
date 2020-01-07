78a0df64dcb75456241812532dc56e113b9cf1e1
Einar Eriksen <gt25@naturviterne.no>
2020-01-07 23:27:55 +0100

    Modified to output data and serve index at /

--- server.js
+++ server.js
@@ -2,21 +2,19 @@
 const express = require('express'); //Import Express
 const app = express(); // Init app
 const path = require('path');
-const bodyParser = require('body-parser');
-
+// const bodyParser = require('body-parser');
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.raw());
+// app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.raw());
 
 //home
-app.get('/index', function (req, res) {
+app.get('/', function (req, res) {
     //res.sendFile(path.join(__dirname + '\\index.html'));
-    res.sendFile(__dirname + '/index');
+    res.sendFile(__dirname + '/index.html');
 });
-
 
 
 app.get('/einar/data', function (req, res) {
@@ -25,7 +23,7 @@ app.get('/einar/data', function (req, res) {
     const sql = require("mssql"); //require the drivers
 
     // Config object for Azure SQL connection
-  const config = require('./config')
+    const config = require('./config')
 
     //Connect to DB
     sql.connect(config.dbConfig, function (err) {
@@ -36,25 +34,36 @@ app.get('/einar/data', function (req, res) {
         let request = new sql.Request();
 
         //query to DB and get record/fields in the data object
-        let sqlQueryAll = 'Select * From lonn2019dummy';
+
+        // let sqlQueryAll = 'SELECT * FROM dbo.lonn2019dummy';
+
+        request.query("SELECT Sektor, AVG(Lønn) AS \"Gjennomsnittslønn\" FROM dbo.lonn2019dummy GROUP BY Sektor UNION ALL SELECT 'Alle' AS Sektor, AVG(Lønn) AS \"Gjennomsnittslønn\" FROM dbo.lonn2019dummy", function (err, result) {
+            if (err) console.log(err)
+            //Display data
+            console.log(result);
+            //Output data
+            res.send(result);
+        });
+
+
 
         //    let sqlQueryKommune = 'select Lønn from lonn2019dummy where Sektor LIKE 'Kommune';');
         // let sqlQueryStat = 'select Lønn from lonn2019dummy where Sektor LIKE 'Stat';';
         // let sqlQueryPriv = 'select Lønn from lonn2019dummy where Sektor LIKE 'Privat';';
 
         //Run the query. Send output to console for now.
-        request.query(sqlQueryAll, function (err, data) {
+        // request.query(sqlQueryAll, function (err, data) {
 
-            if (err) console.log(err)
+        //     if (err) console.log(err)
 
-            //Display data
-            console.log(data);
+        //     //Display data
+        //     console.log(data);
 
-            //res.send(data); //output raw data
-            //close connection
+        //     //res.send(data); //output raw data
+        //     //close connection
 
-            sql.close();
-        });
+        //     sql.close();
+        // });
 
     });
 });
