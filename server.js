3fa9e9023b643c956e3faecaa8e3cc9defd3e3cd
Einar <einareriksen85@gmail.com>
2020-01-24 18:23:48 +0100

    Nye SQL-spørringer, historisk utvikling har riktige historiske tall, gjennomsnitt og median etter sektor på plass

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express(); // Init app
 const router = express.Router();
 const path = require('path');
 const bodyParser = require('body-parser');
-const Highcharts = require('highcharts');
 const rp = require('request-promise');
 const https = require("https")
 const http = require("http")
@@ -23,11 +22,10 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/einar/data', function (req, res) {
-    console.log("GET");
+app.get('/einar/gjdata', function (req, res) {
+    // console.log("GET");
     // Import the mssql package
     const sql = require("mssql"); //require the drivers
-
     // Config object for Azure SQL connection
     const config = require('./config')
     //Connect to DB
@@ -37,18 +35,66 @@ app.get('/einar/data', function (req, res) {
         let request = new sql.Request();
         //query to DB and get record/fields in the data object
         // let sqlQueryAll = 'SELECT * FROM dbo.lonn2019dummy';
-        request.query("SELECT Sektor, AVG(Lønn) AS \"Gjennomsnittslønn\" FROM dbo.lonn2019dummy GROUP BY Sektor UNION ALL SELECT 'Alle' AS Sektor, AVG(Lønn) AS \"Gjennomsnittslønn\" FROM dbo.lonn2019dummy", function (err, result) {
+        request.query("SELECT Sektor, AVG(Årslønn) AS \"Gjennomsnittslønn\" FROM dbo.lonn2019 GROUP BY Sektor UNION ALL SELECT 'Alle' AS Sektor, AVG(Årslønn) AS \"Gjennomsnittslønn\" FROM dbo.lonn2019", function (err, result) {
             if (err) console.log(err)
-            //Display data
-            console.log(result);
-            let recordset = result.recordset;
+            //Display data    
+            // console.log(result);
             //Output data
-            //callback(result);
+            let recordset = result.recordset;
             res.json(recordset);
-            sql.close(); //Trengs det?
+            // sql.close(); //Trengs det?
         });
     });
 });
+
+app.get('/einar/mddata', function (req, res) {
+    // console.log("GET");
+    // Import the mssql package
+    const sql = require("mssql"); //require the drivers
+    // Config object for Azure SQL connection
+    const config = require('./config')
+    //Connect to DB
+    sql.connect(config.dbConfig, function (err) {
+        if (err) console.log(err)
+        //New request object
+        let request = new sql.Request();
+        //query to DB and get record/fields in the data object
+        // let sqlQueryAll = 'SELECT * FROM dbo.lonn2019dummy';
+        request.query("WITH CTE AS \
+        (   SELECT  Sektor, \
+                    Årslønn, \
+                    [half1] = NTILE(2) OVER(PARTITION BY Sektor ORDER BY Årslønn), \
+                    [half2] = NTILE(2) OVER(PARTITION BY Sektor ORDER BY Årslønn DESC), \
+                    [half3] = NTILE(2) OVER(ORDER BY Årslønn), \
+                    [half4] = NTILE(2) OVER(ORDER BY Årslønn DESC) \
+            FROM    dbo.lonn2019 \
+            WHERE   Årslønn IS NOT NULL \
+        ) \
+        SELECT  Sektor, \
+                (MAX(CASE WHEN half1 = 1 THEN Årslønn END) + \
+                MIN(CASE WHEN half2 = 1 THEN Årslønn END)) / 2.0 \
+        AS 'Medianlønn' \
+        FROM    CTE \
+        GROUP BY Sektor \
+        UNION ALL \
+        SELECT 'Alle' \
+        AS Sektor, \
+                (MAX(CASE WHEN half3 = 1 THEN Årslønn END) + \
+                MIN(CASE WHEN half4 = 1 THEN Årslønn END)) / 2.0 \
+        AS 'Medianlønn' \
+        FROM CTE", function (err, result) {
+            if (err) console.log(err)
+            //Display data
+            // console.log(result);
+            //Output data
+            let recordset = result.recordset;
+            res.json(recordset);
+            // sql.close(); //Trengs det?
+        });
+    });
+});
+
+
 
 module.exports = app;
 
@@ -76,3 +122,9 @@ http.createServer(function (req, res) {
     res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
     res.end();
 }).listen(80);
+
+
+// // Bruk lokalt
+// const webserver = app.listen(3000, function () {
+//     console.log('ServsUp (3000)')
+// });
