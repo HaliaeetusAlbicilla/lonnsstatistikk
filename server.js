93eb12e4c31a205823225851fa165ddbd9cd45e4
Einar Eriksen <gt25@naturviterne.no>
2020-01-25 21:34:19 +0100

    bare bytta fra localhost til https

--- server.js
+++ server.js
@@ -36,32 +36,32 @@ app.use("/set2", set2Router)
 
 
 
-// //Letsencrypt Certificate
-// const privateKey = fs.readFileSync('/etc/letsencrypt/live/naturviterne.northeurope.cloudapp.azure.com/privkey.pem', 'utf8');
-// const certificate = fs.readFileSync('/etc/letsencrypt/live/naturviterne.northeurope.cloudapp.azure.com/cert.pem', 'utf8');
-// const ca = fs.readFileSync('/etc/letsencrypt/live/naturviterne.northeurope.cloudapp.azure.com/chain.pem', 'utf8');
+//Letsencrypt Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/naturviterne.northeurope.cloudapp.azure.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/naturviterne.northeurope.cloudapp.azure.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/naturviterne.northeurope.cloudapp.azure.com/chain.pem', 'utf8');
 
-// const credentials = {
-// 	key: privateKey,
-// 	cert: certificate,
-// 	ca: ca
-// };
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
 
-// // // Create an HTTPS service
-// const httpsServer = https.createServer(credentials, app);
+// // Create an HTTPS service
+const httpsServer = https.createServer(credentials, app);
 
-// httpsServer.listen(443, () => {
-// 	console.log('HTTPS Server running on port 443');
-// });
-
-// // create an HTTP server on port 80 and redirect to HTTPS
-// http.createServer(function (req, res) {
-//     res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-//     res.end();
-// }).listen(80);
-
-
-// Bruk lokalt
-const webserver = app.listen(3000, function () {
-    console.log('ServsUp (3000)')
+httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
 });
+
+// create an HTTP server on port 80 and redirect to HTTPS
+http.createServer(function (req, res) {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+}).listen(80);
+
+
+// // Bruk lokalt
+// const webserver = app.listen(3000, function () {
+//     console.log('ServsUp (3000)')
+// });
