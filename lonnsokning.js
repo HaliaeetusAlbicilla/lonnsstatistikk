6ef0a380b97a412d4c213ec436fc8c7085a502d8
Einar Eriksen <gt25@naturviterne.no>
2020-01-29 08:57:02 +0100

    utregning lønnsøkning 2019

--- lonnsokning.js
+++ lonnsokning.js
@@ -1,20 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let medianData = fetch('/set1');
-    let gjData = fetch('/set2')
-    Promise.all([medianData, gjData])
+    let gjData = fetch('/set1')
+    let medianData = fetch('/set2')
+    Promise.all([gjData], [medianData])
         .then((res) => Promise.all(res.map(res => res.json())))
         .then((data) => {
             let mean = data[0];
-            let median = data[1];
 
-            const privatMD = median.find(element => element["Sektor"] === 'Privat');
-            const statMD = median.find(element => element["Sektor"] === 'Stat');
-            const kommuneMD = median.find(element => element["Sektor"] === 'Kommune');
-            const alleMD = median.find(element => element["Sektor"] === 'Alle');
             const privatGJ = mean.find(element => element["Sektor"] === 'Privat');
             const statGJ = mean.find(element => element["Sektor"] === 'Stat');
             const kommuneGJ = mean.find(element => element["Sektor"] === 'Kommune');
             const alleGJ = mean.find(element => element["Sektor"] === 'Alle');
+
+            //Regner ut prosent vekst fra 2018. 
+            //Tallene fra 2018 burde vel egentlig defineres et annet sted
+
+            let kommunediff = kommuneGJ["Gjennomsnittslønn"] - 613846;
+            let kommuneforhold = kommunediff / 613846
+            const kommunevekst = kommuneforhold * 100
+
+            let statdiff = statGJ["Gjennomsnittslønn"] - 605455;
+            let statforhold = statdiff / 605455
+            const statvekst = statforhold * 100
+
+            let privdiff = privatGJ["Gjennomsnittslønn"] - 655355;
+            let privforhold = privdiff / 655355
+            const privvekst = privforhold * 100
+
 
             let optionsMD = {
                 chart: {
@@ -39,8 +50,10 @@ document.addEventListener('DOMContentLoaded', function () {
                 },
                 tooltip: {
                     formatter: function () {
-                        return this.series.name + ': ' + this.y + ' %</b>';
-                    }
+                        return this.series.name + ': ' + this.y.toFixed(1) + ' %</b>';
+
+                    },
+                    valueDecimals: 2,
                 },
                 yAxis: {
                     allowDecimals: false,
@@ -53,19 +66,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
             optionsMD.series.push({
                 name: 'Kommune',
-                data: [3.2, 4.3, 1.7, 4.3, 3.7, 2.9, 1.0],
+                data: [3.2, 4.3, 1.7, 4.3, 3.7, 2.9, kommunevekst],
                 color: '#92bfd5',
             })
 
             optionsMD.series.push({
                 name: 'Stat',
-                data: [1.6, 4.0, 1.1, 3.7, 1.6, 3.5, 1.0],
+                data: [1.6, 4.0, 1.1, 3.7, 1.6, 3.5, statvekst],
                 color: '#94c43a',
             })
 
             optionsMD.series.push({
                 name: 'Privat',
-                data: [2.1, 3.7, 1.9, 3.2, 2.8, 5.4, 1.0],
+                data: [2.1, 3.7, 1.9, 3.2, 2.8, 5.4, privvekst],
                 color: '#F59E00',
             })
 
