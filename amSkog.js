c73a4ef7d753a398f49a9674c06ff694dc2e3fc0
Einar Eriksen <gt25@naturviterne.no>
2020-08-25 14:39:36 +0200

    Flere routes, flere js og flere html er lagt til for å dekke flere utdanningsretninger

---
+++ amSkog.js
@@ -0,0 +1,101 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let scatterData = fetch('/arbSkogfag')
+
+    Promise.all([scatterData])
+        .then((res) => Promise.all(res.map(res => res.json())))
+        .then((data) => {
+            let scatter = data[0]
+            const alle = scatter.map(res => [res.Eksamensår, res.Årslønn])
+
+
+
+
+            //Tallformatering
+            function formatNO(num) {
+                return (
+                    num
+                        .toFixed(0) // ingen desimaltall
+                        .replace('.', ',') // bytt ut . med komma i desimaltall ,
+                        .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
+                ) // bruk . som separator
+            }
+
+            let optionsSC = {
+
+
+
+                chart: {
+
+
+
+                    type: 'scatter',
+                    renderTo: 'container3'
+                },
+
+                lang: {
+                    numericSymbols: null
+                },
+
+                title: {
+                    text: 'Lønn etter eksamensår'
+                },
+                xAxis: {
+
+                    title: {
+                        text: 'Eksamensår'
+                    }
+                },
+                lang: {
+                    numericSymbols: null
+                },
+
+                tooltip: {
+                    formatter: function () {
+                        return 'Eksamensår:' + this.x + ', Årslønn: ' + formatNO(this.y) + ' kroner';
+                    }
+                },
+                yAxis: {
+                    min: 400000,
+                    max: 1000000,
+                    title: {
+                        text: 'Årslønn (NOK)'
+                    },
+                    labels: {
+                        formatter: function () {
+                            return formatNO(this.value);
+                        }
+                    }
+                },
+
+                series: []
+            };
+
+
+            optionsSC.series.push({
+                regression: true,
+                regressionSettings: {
+                    type: 'polynomial',
+                    color: 'black',
+                    lineWidth: 3,
+                },
+                name: 'Medlem',
+                color: '#94c43a',
+                data: alle,
+
+
+            });
+
+
+            // chart.reflow();
+            let chart = new Highcharts.Chart(optionsSC);
+
+        });
+
+
+});
+
+
+
+
+
+
--- routes/arbHusdyrfag.js
+++ routes/arbHusdyrfag.js
@@ -8,14 +8,10 @@ router.get("/", async (req, res) => {
         const result = await pool.request()
             // .input('input_parameter', sql.Int, req.query.input_parameter)
             .query("select CAST(Eksamensår AS INT) as Eksamensår, Årslønn from dbo.lonn2019 \
-            where [Utdanningsretning] like 'Landbruksfag' \
-                or [Utdanningsretning] like 'Husdyrfag' \
-                or [Utdanningsretning] like 'Jordskifte' \
-                or [Utdanningsretning] like 'Husdyrvitenskap' \
-                or [Utdanningsretning] like 'Agroøkologi' \
-                or [Utdanningsretning] like 'Allsidig Jordbruk'")
+            where [Utdanningsretning] like 'Husdyrfag' \
+                or [Utdanningsretning] like 'Husdyrvitenskap' ")
         res.json(result.recordset)
-        console.log(recordset)
+
     } catch (err) {
         res.status(500)
         res.send(err.message)
