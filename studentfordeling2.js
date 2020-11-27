0f5f7428092bd93a27cdf5b2c50ee246c40c038e
Einar <einareriksen85@gmail.com>
2020-11-27 21:52:20 +0100

    En ny route og js-fil for studieretning for alle studentmedlemmer

---
+++ studentfordeling2.js
@@ -0,0 +1,81 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let studentData = fetch('/studentfordeling')
+
+    Promise.all([studentData])
+        .then((res) => Promise.all(res.map(res => res.json())))
+        .then((data) => {
+      
+            let stud = data[0]
+            const studentdata = stud.map(res => [res.utdanningsretning, res.ant])
+
+            //Tallformatering
+            function formatNO(num) {
+                return (
+                    num
+                        .toFixed(0) // ingen desimaltall
+                        .replace('.', ',') // bytt ut . med komma i desimaltall ,
+                        .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
+                ) // bruk . som separator
+            }
+
+
+            let optionsST = {
+
+                chart: {
+                    type: 'pie',
+                    renderTo: 'studentcontainer',
+                },
+
+                lang: {
+                    numericSymbols: null
+                },
+
+                credits: {
+                    enabled: false
+                },
+
+                title: {
+                    text: 'Fordelingen av studentmedlemmer i Naturviterne'
+                },
+                xAxis: {
+
+                    title: {
+                        text: 'Antall'
+                    }
+                },
+                lang: {
+                    numericSymbols: null
+                },
+
+                tooltip: {
+                    formatter: function () {
+                        return 'Antall: ' + formatNO(this.y) + ' medlemmer';
+                    }
+                },
+
+                series: []
+            };
+
+
+        
+            optionsST.series.push({
+                regression: true,
+                regressionSettings: {
+                    type: 'polynomial',
+                    color: 'black',
+                    lineWidth: 3,
+                },
+                name: 'Medlem',
+                color: '#94c43a',
+                data: studentdata,
+            });
+
+let chart = new Highcharts.Chart(optionsST);
+        });
+});
+
+
+
+
+
+
